a52c276e3256b9e9ff57ad92c3469c18e14a5746
brockblaze <brockblazzard@gmail.com>
2018-11-01 11:24:14 -0600

    up

--- assignments/lesson7/scripts/windchill.js
+++ assignments/lesson7/scripts/windchill.js
@@ -1,3 +1,7 @@
 
-function WindChill ()
- var
+  let temperature = parseFloat(document.getElementById('temperature').innerHTML);
+  let s = parseFloat(document.getElementById('windspeed').innerHTML);
+  let windchill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(s,.16) + .4275 * temperature * Math.pow(s,.16);
+
+  
+ document.getElementById("windchill").innerHTML = windchill.toFixed(1);
