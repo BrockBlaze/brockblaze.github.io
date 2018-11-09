c5815a4be51320c479614b24b08eddf1df1589e7
brockblaze <brockblazzard@gmail.com>
2018-11-09 12:17:14 -0700

    up

---
+++ assignments/lesson8/scripts/currentdate.js
@@ -0,0 +1,31 @@
+    
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
+
+    var month = new Array(11);
+    month[0] = "Jan.";
+    month[1] = "Feb.";
+    month[2] = "Mar.";
+    month[3] = "Apr.";
+    month[4] = "May.";
+    month[5] = "Jun.";
+    month[6] = "Jul.";
+    month[7] = "Aug.";
+    month[8] = "Sep.";
+    month[9] = "Oct.";
+    month[10] = "Nov.";
+    month[11] = "Dec.";
+
+
+    var n = weekday[d.getDay()];
+    var m = month[d.getMonth()]
+    var thedate = n + ", " + d.getDate() + " " + m + " " + d.getFullYear();
+    document.getElementById("date").innerHTML = thedate;
+
---
+++ assignments/lesson8/scripts/hamburger.js
@@ -0,0 +1,3 @@
+function toggleHam() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
