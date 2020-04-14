d04a2b96c15f2a488cf062d5718219e42ab84c6f
BrockBlaze <brockblazzard@gmail.com>
2020-04-13 20:44:26 -0600

    a

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,30 @@
 function checkedToggle() {
   var checkBox = document.getElementById("checkbox"); 
   var root = document.documentElement.style;
-  if (checkBox.checked) {
+  if (checkBox.checked){
+    if (typeof(Storage) !== "undefined") {
+      localStorage.setItem("checked", "true");
+    }
+  } else {
+    if (typeof(Storage) !== "undefined") {
+      localStorage.setItem("checked", "false");
+    }
+  }
+  /*if (localStorage.getItem("checked") == "true")
+  {
+    root.setProperty("--bgcolor", "cornflowerblue");
+    root.setProperty("--bdcolor", "rgb(244, 244, 244)");
+    root.setProperty("--fgcolor", "white");
+    root.setProperty("--footercolor", "black");
+  }
+  if (localStorage.getItem("checked") == "false")
+  {
+    root.setProperty("--bgcolor", "rgb(7, 46, 78)");
+    root.setProperty("--bdcolor", "#8b8b8b");
+    root.setProperty("--fgcolor", "rgb(163, 163, 163)");
+    root.setProperty("--footercolor", "white");
+  }*/
+  /*if (checkBox.checked) {
     root.setProperty("--bgcolor", "cornflowerblue");
     root.setProperty("--bdcolor", "rgb(244, 244, 244)");
     root.setProperty("--fgcolor", "white");
@@ -11,5 +34,36 @@ function checkedToggle() {
     root.setProperty("--bdcolor", "#8b8b8b");
     root.setProperty("--fgcolor", "rgb(163, 163, 163)");
     root.setProperty("--footercolor", "white");
+  }*/
+
+  checkToggle();
+
+}
+
+function checkToggle() {
+  var checkBox = document.getElementById("checkbox"); 
+  var toggle = localStorage.getItem("checked");
+  //document.getElementById("result").innerHTML = toggle;
+  var root = document.documentElement.style;
+  if (localStorage.getItem("checked") == "true" && toggle == "true")
+  {
+    root.setProperty("--bgcolor", "cornflowerblue");
+    root.setProperty("--bdcolor", "rgb(244, 244, 244)");
+    root.setProperty("--fgcolor", "white");
+    root.setProperty("--footercolor", "black");
+  }
+  if (localStorage.getItem("checked") == "false" && toggle == "false")
+  {
+    root.setProperty("--bgcolor", "rgb(7, 46, 78)");
+    root.setProperty("--bdcolor", "#8b8b8b");
+    root.setProperty("--fgcolor", "rgb(163, 163, 163)");
+    root.setProperty("--footercolor", "white");
+  }
+  if (toggle == "true")
+  {
+    checkBox.checked = true;
+  }
+  if (toggle == "false"){
+    checkBox.checked = false;
   }
 }
