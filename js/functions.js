6b11b73c10b299269fe72ec411eb3ff7b7335fa7
BrockBlaze <brockblazzard@gmail.com>
2020-04-17 00:40:33 -0600

    a

--- js/functions.js
+++ js/functions.js
@@ -1,69 +1,303 @@
 function checkedToggle() {
-  var checkBox = document.getElementById("checkbox"); 
+  var checkBox = document.getElementById("checkbox");
   var root = document.documentElement.style;
-  if (checkBox.checked){
-    if (typeof(Storage) !== "undefined") {
+  if (checkBox.checked) {
+    if (typeof Storage !== "undefined") {
       localStorage.setItem("checked", "true");
     }
   } else {
-    if (typeof(Storage) !== "undefined") {
+    if (typeof Storage !== "undefined") {
       localStorage.setItem("checked", "false");
     }
   }
-  /*if (localStorage.getItem("checked") == "true")
-  {
-    root.setProperty("--bgcolor", "cornflowerblue");
-    root.setProperty("--bdcolor", "rgb(244, 244, 244)");
-    root.setProperty("--fgcolor", "white");
-    root.setProperty("--footercolor", "black");
-  }
-  if (localStorage.getItem("checked") == "false")
-  {
-    root.setProperty("--bgcolor", "rgb(7, 46, 78)");
-    root.setProperty("--bdcolor", "#8b8b8b");
-    root.setProperty("--fgcolor", "rgb(163, 163, 163)");
-    root.setProperty("--footercolor", "white");
-  }*/
-  /*if (checkBox.checked) {
-    root.setProperty("--bgcolor", "cornflowerblue");
-    root.setProperty("--bdcolor", "rgb(244, 244, 244)");
-    root.setProperty("--fgcolor", "white");
-    root.setProperty("--footercolor", "black");
-  } else {
-    root.setProperty("--bgcolor", "rgb(7, 46, 78)");
-    root.setProperty("--bdcolor", "#8b8b8b");
-    root.setProperty("--fgcolor", "rgb(163, 163, 163)");
-    root.setProperty("--footercolor", "white");
-  }*/
-
   checkToggle();
+}
 
+if (!window.getComputedStyle) {
+  // Fallback for obsolete IE
+  window.getComputedStyle = function (e) {
+    return e.currentStyle;
+  };
+}
+
+function ham() {
+  var nav = document.getElementById("navB");
+  if (getComputedStyle(nav).display === "none") {
+    nav.style.display = "flex";
+  } else {
+    nav.style.display = "none";
+  }
+}
+
+function resize() {
+  var nav = document.getElementById("navB");
+  if (window.outerWidth > 700) {
+    nav.style.display = "flex";
+  }
+  if (window.outerWidth < 700) {
+    nav.style.display = "none";
+  }
 }
 
 function checkToggle() {
-  var checkBox = document.getElementById("checkbox"); 
+  var checkBox = document.getElementById("checkbox");
   var toggle = localStorage.getItem("checked");
   //document.getElementById("result").innerHTML = toggle;
   var root = document.documentElement.style;
-  if (localStorage.getItem("checked") == "true" && toggle == "true")
-  {
+  if (localStorage.getItem("checked") == "true" && toggle == "true") {
     root.setProperty("--bgcolor", "cornflowerblue");
     root.setProperty("--bdcolor", "rgb(244, 244, 244)");
     root.setProperty("--fgcolor", "white");
     root.setProperty("--footercolor", "black");
   }
-  if (localStorage.getItem("checked") == "false" && toggle == "false")
-  {
+  if (localStorage.getItem("checked") == "false" && toggle == "false") {
     root.setProperty("--bgcolor", "rgb(7, 46, 78)");
     root.setProperty("--bdcolor", "#8b8b8b");
     root.setProperty("--fgcolor", "rgb(163, 163, 163)");
     root.setProperty("--footercolor", "white");
   }
-  if (toggle == "true")
-  {
+  if (toggle == "true") {
     checkBox.checked = true;
   }
-  if (toggle == "false"){
+  if (toggle == "false") {
     checkBox.checked = false;
+  }
+
+  var newPlayer = document.getElementById("newPlayer");
+  var gameMenu = document.getElementById("gameMenu");
+  var newPlayerCreation = document.getElementById("newPlayerCreation");
+  var gamePlay = document.getElementById("gamePlay");
+  var stats = document.getElementById("stats");
+  var fight = document.getElementById("fight");
+  var town = document.getElementById("town");
+  var home = document.getElementById("home");
+  if (!localStorage.getItem("name") && !localStorage.getItem("race")) {
+    newPlayerCreation.style.display = "none";
+    gameMenu.style.display = "none";
+    gamePlay.style.display = "none";
+    stats.style.display = "none";
+    fight.style.display = "none";
+    town.style.display = "none";
+    home.style.display = "none";
+  }
+  if (localStorage.getItem("name") && !localStorage.getItem("race")) {
+    newPlayer.style.display = "none";
+    gameMenu.style.display = "none";
+    gamePlay.style.display = "none";
+    stats.style.display = "none";
+    fight.style.display = "none";
+    town.style.display = "none";
+    home.style.display = "none";
+  }
+  if (localStorage.getItem("name") && localStorage.getItem("race")) {
+    document.getElementById("output").innerHTML =
+      "Welcome " + localStorage.getItem("name");
+    newPlayer.style.display = "none";
+    newPlayerCreation.style.display = "none";
+    gamePlay.style.display = "none";
+    stats.style.display = "none";
+    fight.style.display = "none";
+    town.style.display = "none";
+    home.style.display = "none";
+  }
+}
+
+function EnterName() {
+  var name = document.getElementById("name").value;
+  if (name.value == "") {
+    document.getElementById("output").innerHTML = "Please enter a Name";
+  } else {
+    storeName(name);
+    location.reload();
+    return false;
+  }
+}
+
+function EnterRace() {
+  var race = document.getElementById("race").value;
+  storeRace(race);
+  location.reload();
+  return false;
+}
+
+function storeName(name) {
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("name", name);
+  }
+}
+
+function storeRace(race) {
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("race", race);
+    localStorage.setItem("health", 100)
+    localStorage.setItem("level", 1);
+    localStorage.setItem("gold", 0)
+  }
+}
+
+function DeletePlayer() {
+  if (typeof Storage !== "undefined") {
+    localStorage.removeItem("name");
+    localStorage.removeItem("race");
+  }
+  location.reload();
+  return false;
+}
+
+function mainMenu() {
+  location.reload();
+  return false;
+}
+
+function Play() {
+  var newPlayer = document.getElementById("newPlayer");
+  var gameMenu = document.getElementById("gameMenu");
+  var newPlayerCreation = document.getElementById("newPlayerCreation");
+  var gamePlay = document.getElementById("gamePlay");
+  var stats = document.getElementById("stats");
+  var fight = document.getElementById("fight");
+  var town = document.getElementById("town");
+  var home = document.getElementById("home");
+  document.getElementById("nameRace").innerHTML = localStorage.getItem("name") + " the " + localStorage.getItem("race");
+  document.getElementById("yourHealth").innerHTML = "Your health - " + localStorage.getItem("health");
+  document.getElementById("yourLevel").innerHTML = "Your Level - " + localStorage.getItem("level");
+  document.getElementById("yourGold").innerHTML = "Your Gold - " + localStorage.getItem("gold");
+  newPlayer.style.display = "none";
+  newPlayerCreation.style.display = "none";
+  gameMenu.style.display = "none";
+  stats.style.display = "none";
+  fight.style.display = "none";
+  town.style.display = "none";
+  home.style.display = "none";
+  gamePlay.style.display = "block";
+}
+
+function Stats() {
+  var newPlayer = document.getElementById("newPlayer");
+  var gameMenu = document.getElementById("gameMenu");
+  var newPlayerCreation = document.getElementById("newPlayerCreation");
+  var gamePlay = document.getElementById("gamePlay");
+  var stats = document.getElementById("stats");
+  var fight = document.getElementById("fight");
+  var town = document.getElementById("town");
+  var home = document.getElementById("home");
+  document.getElementById("nameRace1").innerHTML = localStorage.getItem("name") + " the " + localStorage.getItem("race");
+  document.getElementById("yourHealth1").innerHTML = "Your health - " + localStorage.getItem("health");
+  document.getElementById("yourLevel1").innerHTML = "Your Level - " + localStorage.getItem("level");
+  document.getElementById("yourGold1").innerHTML = "Your Gold - " + localStorage.getItem("gold");
+  newPlayer.style.display = "none";
+  newPlayerCreation.style.display = "none";
+  gameMenu.style.display = "none";
+  stats.style.display = "block";
+  fight.style.display = "none";
+  town.style.display = "none";
+  home.style.display = "none";
+  gamePlay.style.display = "none";
+}
+
+function updateGold(gold) {
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("gold", gold);
+  }
+}
+
+function Fight() {
+  var newPlayer = document.getElementById("newPlayer");
+  var gameMenu = document.getElementById("gameMenu");
+  var newPlayerCreation = document.getElementById("newPlayerCreation");
+  var gamePlay = document.getElementById("gamePlay");
+  var stats = document.getElementById("stats");
+  var fight = document.getElementById("fight");
+  var town = document.getElementById("town");
+  var home = document.getElementById("home");
+  newPlayer.style.display = "none";
+  newPlayerCreation.style.display = "none";
+  gameMenu.style.display = "none";
+  stats.style.display = "none";
+  fight.style.display = "block";
+  town.style.display = "none";
+  home.style.display = "none";
+  gamePlay.style.display = "none";
+  var r = Math.round(Math.random() * 10);
+  var c = Math.round(Math.random() * 10);
+  if (r <= 7)
+  {
+    if (c <= 5)
+    {
+      document.getElementById("fightoutput").innerHTML = "A Wolf approaches";
+      if (localStorage.getItem("level") > 0)
+      {
+        if (c <= 2) {
+          document.getElementById("fightoutput").innerHTML = "You Slaughter the Wolf and Found " + r + " gold";
+          localStorage.setItem("gold") += r;
+        }else {
+          document.getElementById("fightoutput").innerHTML = "You Slaughter the Wolf and Found nothing";
+        }
+      }
+    }else {
+      document.getElementById("fightoutput").innerHTML = "An Orc approaches";
+    }
+  }else {
+    document.getElementById("fightoutput").innerHTML = "Nothing found";
+  }
+}
+
+function Town() {
+  var newPlayer = document.getElementById("newPlayer");
+  var gameMenu = document.getElementById("gameMenu");
+  var newPlayerCreation = document.getElementById("newPlayerCreation");
+  var gamePlay = document.getElementById("gamePlay");
+  var stats = document.getElementById("stats");
+  var fight = document.getElementById("fight");
+  var town = document.getElementById("town");
+  var home = document.getElementById("home");
+  newPlayer.style.display = "none";
+  newPlayerCreation.style.display = "none";
+  gameMenu.style.display = "none";
+  stats.style.display = "none";
+  fight.style.display = "none";
+  town.style.display = "block";
+  home.style.display = "none";
+  gamePlay.style.display = "none";
+  var c = Math.round(Math.random() * 10);
+  var r = Math.round(Math.random() * 10);
+  if (r <= 7)
+  {
+    if (c <= 5)
+    {
+
+    }
+  }else {
+
+  }
+}
+
+function Home() {
+  var newPlayer = document.getElementById("newPlayer");
+  var gameMenu = document.getElementById("gameMenu");
+  var newPlayerCreation = document.getElementById("newPlayerCreation");
+  var gamePlay = document.getElementById("gamePlay");
+  var stats = document.getElementById("stats");
+  var fight = document.getElementById("fight");
+  var town = document.getElementById("town");
+  var home = document.getElementById("home");
+  newPlayer.style.display = "none";
+  newPlayerCreation.style.display = "none";
+  gameMenu.style.display = "none";
+  stats.style.display = "none";
+  fight.style.display = "none";
+  town.style.display = "none";
+  home.style.display = "block";
+  gamePlay.style.display = "none";
+  var c = Math.round(Math.random() * 10);
+  var r = Math.round(Math.random() * 10);
+  if (r <= 7)
+  {
+    if (c <= 5)
+    {
+
+    }
+  }else {
+
   }
 }
--- js/game.js
+++ js/game.js
@@ -1,11 +1,41 @@
+
 var myGamePiece;
 var myObstacles = [];
 var myScore;
 
-function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece.gravity = 0.05;
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+document.addEventListener("keydown", function(event) {
+    if (event.code == 'KeyW') {
+      moveup();
+    }
+    if (event.code == 'KeyS') {
+      movedown();
+    }
+    if (event.code == 'KeyA') {
+      moveleft();
+    }
+    if (event.code == 'KeyD') {
+      moveright();
+    }      
+});
+
+document.addEventListener("keyup", function(event) {
+    if (event.code == 'KeyW') {
+      movedown();
+    }
+    if (event.code == 'KeyS') {
+      moveup();
+    }
+    if (event.code == 'KeyA') {
+      moveright();
+    }
+    if (event.code == 'KeyD') {
+      moveleft();
+    }      
+});
+
+
+function startGame() {  
+    myGamePiece = new component(10, 10, "red", 230, 120);
     myGameArea.start();
 }
 
@@ -15,102 +45,51 @@ var myGameArea = {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        document.body.insertBefore(this.canvas, document.body.childNodes[5]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameArea, 30);
         },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.score = 0;
+function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;    
+    this.speedY = 0;
     this.x = x;
-    this.y = y;
-    this.gravity = 0;
-    this.gravitySpeed = 0;
+    this.y = y;    
     this.update = function() {
         ctx = myGameArea.context;
-        if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
-            ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
     this.newPos = function() {
-        this.gravitySpeed += this.gravity;
         this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
-        }
-    }
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
+        this.y += this.speedY;        
+    }    
 }
 
 function updateGameArea() {
-    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < myObstacles.length; i += 1) {
-        if (myGamePiece.crashWith(myObstacles[i])) {
-            return;
-        } 
-    }
     myGameArea.clear();
-    myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(150)) {
-        x = myGameArea.canvas.width;
-        minHeight = 20;
-        maxHeight = 200;
-        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-        minGap = 50;
-        maxGap = 200;
-        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "green", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
-    }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
-    }
-    myScore.text="SCORE: " + myGameArea.frameNo;
-    myScore.update();
-    myGamePiece.newPos();
+    myGamePiece.newPos();    
     myGamePiece.update();
 }
 
-function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
+function moveup() {
+    myGamePiece.speedY -= 1; 
 }
 
-function accelerate(n) {
-    myGamePiece.gravity = n;
+function movedown() {
+    myGamePiece.speedY += 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1; 
+}
+
+function moveright() {
+    myGamePiece.speedX += 1; 
 }
