5073a2ba115b1bfb57b3901e20c0eb3a29488156
BrockBlaze <brockblazzard@gmail.com>
2020-06-07 21:12:38 -0600

    a

--- js/bomJep.js
+++ js/bomJep.js
@@ -3,24 +3,43 @@ var m1count = 1;
 var a1count = 1;
 var h1count = 1;
 var i1count = 1;
+var n2count = 1;
+var m2count = 1;
+var a2count = 1;
+var h2count = 1;
+var i2count = 1;
+var n3count = 1;
+var m3count = 1;
+var a3count = 1;
+var h3count = 1;
+var i3count = 1;
+var n4count = 1;
+var m4count = 1;
+var a4count = 1;
+var h4count = 1;
+var i4count = 1;
+var n5count = 1;
+var m5count = 1;
+var a5count = 1;
+var h5count = 1;
+var i5count = 1;
 
-setInterval(Debug,1000);
 
 function n1() {
     
     if (n1count == 0)
     {
-        document.getElementById("n1").style.fontSize = "56px";
+        document.getElementById("n1").style.fontSize = "3rem";
         document.getElementById("n1").innerHTML = "100";
     }
     if (n1count == 1)
     {
-        document.getElementById("n1").style.fontSize = "24px";
-        document.getElementById("n1").innerHTML = "This is a Question?";
+        document.getElementById("n1").style.fontSize = "1.5rem";
+        document.getElementById("n1").innerHTML = "Christ's refers to this OT prophet in 3 Nephi";
     }
     if (n1count == 2)
     {
-        document.getElementById("n1").innerHTML = "Answer";
+        document.getElementById("n1").innerHTML = "What is Isaiah?";
     }
     if (n1count == 3)
     {
@@ -33,17 +52,17 @@ function m1() {
     
     if (m1count == 0)
     {
-        document.getElementById("m1").style.fontSize = "56px";
+        document.getElementById("m1").style.fontSize = "3rem";
         document.getElementById("m1").innerHTML = "100";
     }
     if (m1count == 1)
     {
-        document.getElementById("m1").style.fontSize = "24px";
-        document.getElementById("m1").innerHTML = "This is a Question?";
+        document.getElementById("m1").style.fontSize = "1.5rem";
+        document.getElementById("m1").innerHTML = "A variety of ways to read text";
     }
     if (m1count == 2)
     {
-        document.getElementById("m1").innerHTML = "Answer";
+        document.getElementById("m1").innerHTML = "What is Methodologies?";
     }
     if (m1count == 3)
     {
@@ -56,17 +75,17 @@ function a1() {
     
     if (a1count == 0)
     {
-        document.getElementById("a1").style.fontSize = "56px";
+        document.getElementById("a1").style.fontSize = "3rem";
         document.getElementById("a1").innerHTML = "100";
     }
     if (a1count == 1)
     {
-        document.getElementById("a1").style.fontSize = "24px";
-        document.getElementById("a1").innerHTML = "This is a Question?";
+        document.getElementById("a1").style.fontSize = "1.5rem";
+        document.getElementById("a1").innerHTML = "Doctrine of Jesus Christ.";
     }
     if (a1count == 2)
     {
-        document.getElementById("a1").innerHTML = "Answer";
+        document.getElementById("a1").innerHTML = "What is Faith, Repentance, Baptism, Gift of the Holy Ghost, Enduring to the End?";
     }
     if (a1count == 3)
     {
@@ -79,12 +98,12 @@ function h1() {
     
     if (h1count == 0)
     {
-        document.getElementById("h1").style.fontSize = "56px";
+        document.getElementById("h1").style.fontSize = "3rem";
         document.getElementById("h1").innerHTML = "100";
     }
     if (h1count == 1)
     {
-        document.getElementById("h1").style.fontSize = "24px";
+        document.getElementById("h1").style.fontSize = "1.5rem";
         document.getElementById("h1").innerHTML = "This is a Question?";
     }
     if (h1count == 2)
@@ -102,17 +121,17 @@ function i1() {
     
     if (i1count == 0)
     {
-        document.getElementById("i1").style.fontSize = "56px";
+        document.getElementById("i1").style.fontSize = "3rem";
         document.getElementById("i1").innerHTML = "100";
     }
     if (i1count == 1)
     {
-        document.getElementById("i1").style.fontSize = "24px";
-        document.getElementById("i1").innerHTML = "This is a Question?";
+        document.getElementById("i1").style.fontSize = "1.5rem";
+        document.getElementById("i1").innerHTML = "Lehi's Wife";
     }
     if (i1count == 2)
     {
-        document.getElementById("i1").innerHTML = "Answer";
+        document.getElementById("i1").innerHTML = "What is Sariah?";
     }
     if (i1count == 3)
     {
@@ -120,6 +139,476 @@ function i1() {
     }
     i1count += 1;
 }
+
+//--------------------------------------------------
+
+function n2() {
+    
+    if (n2count == 0)
+    {
+        document.getElementById("n2").style.fontSize = "3rem";
+        document.getElementById("n2").innerHTML = "200";
+    }
+    if (n2count == 1)
+    {
+        document.getElementById("n2").style.fontSize = "1.5rem";
+        document.getElementById("n2").innerHTML = "Setting of King Benjamin's discourse";
+    }
+    if (n2count == 2)
+    {
+        document.getElementById("n2").innerHTML = "What is Temple Setting?";
+    }
+    if (n2count == 3)
+    {
+        document.getElementById("n2").innerHTML = "Done";
+    }
+    n2count ++;
+}
+
+function m2() {
+    
+    if (m2count == 0)
+    {
+        document.getElementById("m2").style.fontSize = "3rem";
+        document.getElementById("m2").innerHTML = "200";
+    }
+    if (m2count == 1)
+    {
+        document.getElementById("m2").style.fontSize = "1.5rem";
+        document.getElementById("m2").innerHTML = "Number of methodologies discussed in this class";
+    }
+    if (m2count == 2)
+    {
+        document.getElementById("m2").innerHTML = "What is Two?";
+    }
+    if (m2count == 3)
+    {
+        document.getElementById("m2").innerHTML = "Done";
+    }
+    m2count += 1;
+}
+
+function a2() {
+    
+    if (a2count == 0)
+    {
+        document.getElementById("a2").style.fontSize = "3rem";
+        document.getElementById("a2").innerHTML = "200";
+    }
+    if (a2count == 1)
+    {
+        document.getElementById("a2").style.fontSize = "1.5rem";
+        document.getElementById("a2").innerHTML = "Chapter that talks of the Fall";
+    }
+    if (a2count == 2)
+    {
+        document.getElementById("a2").innerHTML = "What is 2 Nephi 2?";
+    }
+    if (a2count == 3)
+    {
+        document.getElementById("a2").innerHTML = "Done";
+    }
+    a2count += 1;
+}
+
+function h2() {
+    
+    if (h2count == 0)
+    {
+        document.getElementById("h2").style.fontSize = "3rem";
+        document.getElementById("h2").innerHTML = "200";
+    }
+    if (h2count == 1)
+    {
+        document.getElementById("h2").style.fontSize = "1.5rem";
+        document.getElementById("h2").innerHTML = "This is a Question?";
+    }
+    if (h2count == 2)
+    {
+        document.getElementById("h2").innerHTML = "Answer";
+    }
+    if (h2count == 3)
+    {
+        document.getElementById("h2").innerHTML = "Done";
+    }
+    h2count += 1;
+}
+
+function i2() {
+    
+    if (i2count == 0)
+    {
+        document.getElementById("i2").style.fontSize = "3rem";
+        document.getElementById("i2").innerHTML = "200";
+    }
+    if (i2count == 1)
+    {
+        document.getElementById("i2").style.fontSize = "1.5rem";
+        document.getElementById("i2").innerHTML = "Authors of the Book of Mormon";
+    }
+    if (i2count == 2)
+    {
+        document.getElementById("i2").innerHTML = "What is Nephi, Mormon, Moroni?";
+    }
+    if (i2count == 3)
+    {
+        document.getElementById("i2").innerHTML = "Done";
+    }
+    i2count += 1;
+}
+
+//-----------------------------------------------------
+
+function n3() {
+    
+    if (n3count == 0)
+    {
+        document.getElementById("n3").style.fontSize = "3rem";
+        document.getElementById("n3").innerHTML = "300";
+    }
+    if (n3count == 1)
+    {
+        document.getElementById("n3").style.fontSize = "1.5rem";
+        document.getElementById("n3").innerHTML = "Taught about Christ's atoning sacrifice";
+    }
+    if (n3count == 2)
+    {
+        document.getElementById("n3").innerHTML = "What is King Benjamin?";
+    }
+    if (n3count == 3)
+    {
+        document.getElementById("n3").innerHTML = "Done";
+    }
+    n3count ++;
+}
+
+function m3() {
+    
+    if (m3count == 0)
+    {
+        document.getElementById("m3").style.fontSize = "3rem";
+        document.getElementById("m3").innerHTML = "300";
+    }
+    if (m3count == 1)
+    {
+        document.getElementById("m3").style.fontSize = "1.5rem";
+        document.getElementById("m3").innerHTML = "A method or theory of interpretation";
+    }
+    if (m3count == 2)
+    {
+        document.getElementById("m3").innerHTML = "What is Hermeneutic?";
+    }
+    if (m3count == 3)
+    {
+        document.getElementById("m3").innerHTML = "Done";
+    }
+    m3count += 1;
+}
+
+function a3() {
+    
+    if (a3count == 0)
+    {
+        document.getElementById("a3").style.fontSize = "3rem";
+        document.getElementById("a3").innerHTML = "300";
+    }
+    if (a3count == 1)
+    {
+        document.getElementById("a3").style.fontSize = "1.5rem";
+        document.getElementById("a3").innerHTML = "Bringing two this together";
+    }
+    if (a3count == 2)
+    {
+        document.getElementById("a3").innerHTML = "What is the Atonement?";
+    }
+    if (a3count == 3)
+    {
+        document.getElementById("a3").innerHTML = "Done";
+    }
+    a3count += 1;
+}
+
+function h3() {
+    
+    if (h3count == 0)
+    {
+        document.getElementById("h3").style.fontSize = "3rem";
+        document.getElementById("h3").innerHTML = "300";
+    }
+    if (h3count == 1)
+    {
+        document.getElementById("h3").style.fontSize = "1.5rem";
+        document.getElementById("h3").innerHTML = "This is a Question?";
+    }
+    if (h3count == 2)
+    {
+        document.getElementById("h3").innerHTML = "Answer";
+    }
+    if (h3count == 3)
+    {
+        document.getElementById("h3").innerHTML = "Done";
+    }
+    h3count += 1;
+}
+
+function i3() {
+    
+    if (i3count == 0)
+    {
+        document.getElementById("i3").style.fontSize = "3rem";
+        document.getElementById("i3").innerHTML = "300";
+    }
+    if (i3count == 1)
+    {
+        document.getElementById("i3").style.fontSize = "1.5rem";
+        document.getElementById("i3").innerHTML = "Lehi's Wife";
+    }
+    if (i3count == 2)
+    {
+        document.getElementById("i3").innerHTML = "What is Sariah?";
+    }
+    if (i3count == 3)
+    {
+        document.getElementById("i3").innerHTML = "Done";
+    }
+    i3count += 1;
+}
+
+//-----------------------------------------------------
+
+function n4() {
+    
+    if (n4count == 0)
+    {
+        document.getElementById("n4").style.fontSize = "3rem";
+        document.getElementById("n4").innerHTML = "400";
+    }
+    if (n4count == 1)
+    {
+        document.getElementById("n4").style.fontSize = "1.5rem";
+        document.getElementById("n4").innerHTML = "Sign from Christ of the gathering of Israel";
+    }
+    if (n4count == 2)
+    {
+        document.getElementById("n4").innerHTML = "What is the coming forth of the Book of Mormon?";
+    }
+    if (n4count == 3)
+    {
+        document.getElementById("n4").innerHTML = "Done";
+    }
+    n4count ++;
+}
+
+function m4() {
+    
+    if (m4count == 0)
+    {
+        document.getElementById("m4").style.fontSize = "3rem";
+        document.getElementById("m4").innerHTML = "400";
+    }
+    if (m4count == 1)
+    {
+        document.getElementById("m4").style.fontSize = "1.5rem";
+        document.getElementById("m4").innerHTML = "Interpretation of a text that takes into account the context of its original author.";
+    }
+    if (m4count == 2)
+    {
+        document.getElementById("m4").innerHTML = "What is Exegesis?";
+    }
+    if (m4count == 3)
+    {
+        document.getElementById("m4").innerHTML = "Done";
+    }
+    m4count += 1;
+}
+
+function a4() {
+    
+    if (a4count == 0)
+    {
+        document.getElementById("a4").style.fontSize = "3rem";
+        document.getElementById("a4").innerHTML = "400";
+    }
+    if (a4count == 1)
+    {
+        document.getElementById("a4").style.fontSize = "1.5rem";
+        document.getElementById("a4").innerHTML = "Number of Resurrections";
+    }
+    if (a4count == 2)
+    {
+        document.getElementById("a4").innerHTML = "What is Two?";
+    }
+    if (a4count == 3)
+    {
+        document.getElementById("a4").innerHTML = "Done";
+    }
+    a4count += 1;
+}
+
+function h4() {
+    
+    if (h4count == 0)
+    {
+        document.getElementById("h4").style.fontSize = "3rem";
+        document.getElementById("h4").innerHTML = "400";
+    }
+    if (h4count == 1)
+    {
+        document.getElementById("h4").style.fontSize = "1.5rem";
+        document.getElementById("h4").innerHTML = "This is a Question?";
+    }
+    if (h4count == 2)
+    {
+        document.getElementById("h4").innerHTML = "Answer";
+    }
+    if (h4count == 3)
+    {
+        document.getElementById("h4").innerHTML = "Done";
+    }
+    h4count += 1;
+}
+
+function i4() {
+    
+    if (i4count == 0)
+    {
+        document.getElementById("i4").style.fontSize = "3rem";
+        document.getElementById("i4").innerHTML = "400";
+    }
+    if (i4count == 1)
+    {
+        document.getElementById("i4").style.fontSize = "1.5rem";
+        document.getElementById("i4").innerHTML = "Lehi's Wife";
+    }
+    if (i4count == 2)
+    {
+        document.getElementById("i4").innerHTML = "What is Sariah?";
+    }
+    if (i4count == 3)
+    {
+        document.getElementById("i4").innerHTML = "Done";
+    }
+    i4count += 1;
+}
+
+//-----------------------------------------------------
+
+function n5() {
+    
+    if (n5count == 0)
+    {
+        document.getElementById("n5").style.fontSize = "3rem";
+        document.getElementById("n5").innerHTML = "500";
+    }
+    if (n5count == 1)
+    {
+        document.getElementById("n5").style.fontSize = "1.5rem";
+        document.getElementById("n5").innerHTML = "Jacob's two main problems with the Nephites";
+    }
+    if (n5count == 2)
+    {
+        document.getElementById("n5").innerHTML = "What is lifted up in pride and searching for gold and silver?";
+    }
+    if (n5count == 3)
+    {
+        document.getElementById("n5").innerHTML = "Done";
+    }
+    n5count ++;
+}
+
+function m5() {
+    
+    if (m5count == 0)
+    {
+        document.getElementById("m5").style.fontSize = "3rem";
+        document.getElementById("m5").innerHTML = "500";
+    }
+    if (m5count == 1)
+    {
+        document.getElementById("m5").style.fontSize = "1.5rem";
+        document.getElementById("m5").innerHTML = "Interpretation of a scriptural text from a reader's perspective";
+    }
+    if (m5count == 2)
+    {
+        document.getElementById("m5").innerHTML = "What is Eisegesis?";
+    }
+    if (m5count == 3)
+    {
+        document.getElementById("m5").innerHTML = "Done";
+    }
+    m5count += 1;
+}
+
+function a5() {
+    
+    if (a5count == 0)
+    {
+        document.getElementById("a5").style.fontSize = "3rem";
+        document.getElementById("a5").innerHTML = "500";
+    }
+    if (a5count == 1)
+    {
+        document.getElementById("a5").style.fontSize = "1.5rem";
+        document.getElementById("a5").innerHTML = "This is a Question?";
+    }
+    if (a5count == 2)
+    {
+        document.getElementById("a5").innerHTML = "Answer";
+    }
+    if (a5count == 3)
+    {
+        document.getElementById("a5").innerHTML = "Done";
+    }
+    a5count += 1;
+}
+
+function h5() {
+    
+    if (h5count == 0)
+    {
+        document.getElementById("h5").style.fontSize = "3rem";
+        document.getElementById("h5").innerHTML = "500";
+    }
+    if (h5count == 1)
+    {
+        document.getElementById("h5").style.fontSize = "1.5rem";
+        document.getElementById("h5").innerHTML = "This is a Question?";
+    }
+    if (h5count == 2)
+    {
+        document.getElementById("h5").innerHTML = "Answer";
+    }
+    if (h5count == 3)
+    {
+        document.getElementById("h5").innerHTML = "Done";
+    }
+    h5count += 1;
+}
+
+function i5() {
+    
+    if (i5count == 0)
+    {
+        document.getElementById("i5").style.fontSize = "3rem";
+        document.getElementById("i5").innerHTML = "500";
+    }
+    if (i5count == 1)
+    {
+        document.getElementById("i5").style.fontSize = "1.5rem";
+        document.getElementById("i5").innerHTML = "Lehi's Wife";
+    }
+    if (i5count == 2)
+    {
+        document.getElementById("i5").innerHTML = "What is Sariah?";
+    }
+    if (i5count == 3)
+    {
+        document.getElementById("i5").innerHTML = "Done";
+    }
+    i5count += 1;
+}
+
+//-----------------------------------------------------
 
 function n1reset() {
     n1count = 0;
@@ -141,6 +630,92 @@ function i1reset() {
     i1count = 0;
 }
 
-function Debug() {
-    document.getElementById("debug").innerHTML = "";
+//----------------------------------------------------
+
+function n2reset() {
+    n2count = 0;
 }
+
+function m2reset() {
+    m2count = 0;
+}
+
+function a2reset() {
+    a2count = 0;
+}
+
+function h2reset() {
+    h2count = 0;
+}
+
+function i2reset() {
+    i2count = 0;
+}
+
+//----------------------------------------------------
+
+function n3reset() {
+    n3count = 0;
+}
+
+function m3reset() {
+    m3count = 0;
+}
+
+function a3reset() {
+    a3count = 0;
+}
+
+function h3reset() {
+    h3count = 0;
+}
+
+function i3reset() {
+    i3count = 0;
+}
+
+//----------------------------------------------------
+
+function n4reset() {
+    n4count = 0;
+}
+
+function m4reset() {
+    m4count = 0;
+}
+
+function a4reset() {
+    a4count = 0;
+}
+
+function h4reset() {
+    h4count = 0;
+}
+
+function i4reset() {
+    i4count = 0;
+}
+
+//----------------------------------------------------
+
+function n5reset() {
+    n5count = 0;
+}
+
+function m5reset() {
+    m5count = 0;
+}
+
+function a5reset() {
+    a5count = 0;
+}
+
+function h5reset() {
+    h5count = 0;
+}
+
+function i5reset() {
+    i5count = 0;
+}
+
+//----------------------------------------------------
