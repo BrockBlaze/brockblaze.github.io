96df6ffed6dd6bf78713865a4eadac9ed0d0dbc0
BrockBlaze <brockbazzard@gmail.com>
2023-04-18 23:42:25 -0600

    a

--- Solace/solace.js
+++ Solace/solace.js
@@ -1,2 +1,84 @@
 var mainMenuAudio = document.getElementById('StartAudio');
 mainMenuAudio.volume = 0.1;
+
+var startScreen = document.getElementById('StartScreen');
+var myGamePiece;
+
+function startGame() {
+    startScreen.style.display = 'none';
+    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();    
+    myGamePiece.update();
+}
+
+window.addEventListener("keydown", function (event) {
+    if (event.key === "w")
+    {
+        moveup();
+    }
+    if (event.key === "s")
+    {
+        movedown();
+    }
+    if (event.key === "a")
+    {
+        moveleft();
+    }
+    if (event.key === "d")
+    {
+        moveright();
+    }
+});
+
+function moveup() {
+    myGamePiece.speedY -= 1; 
+}
+
+function movedown() {
+    myGamePiece.speedY += 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1; 
+}
+
+function moveright() {
+    myGamePiece.speedX += 1; 
+}
