d3bdc31b0025339063873d3933ad0d526c3ec4bb
BrockBlaze <brockbazzard@gmail.com>
2023-07-08 13:54:15 -0600

    a

--- Wedding/wedding.js
+++ Wedding/wedding.js
@@ -1,35 +1,33 @@
-var token = '11AHWIFUY0TP2FRGiRq8id_bVpCun6ia1TPeb2j7HQXVUFe2ccnfkF8bcgBTDsJ3SZGF7GTA6ML58a7jLh';
-var t = '00';
+var token = '11AHWIFUY07BcxrUFwB16p_97c6vUc1EaZnBAptLov8x7p26xKCJMZeX4fawnvEwYeIEZC2JOKtId50bbs';
+var t = 'a';
 
-async function uploadPhoto() {
-  const photoInput = document.getElementById("photoInput");
+function uploadPhoto() {
+  const photoInput = document.getElementById('photoInput');
   const file = photoInput.files[0];
 
   if (!file) {
-    console.error("No file selected");
+    console.error('No file selected');
     return;
   }
 
-  t = token.replace('2', '9')
-  const accessToken = "github_pat_" + t;
-  console.log(accessToken);
-  const repoOwner = "BrockBlaze";
-  const repoName = "brockblaze.github.io";
-  const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${file.name}`;
+  t = token.replace('r', 'Q');
+
+  const accessToken = 'github_pat_' + t;
+  const repoOwner = 'BrockBlaze';
+  const repoName = 'brockblaze.github.io';
+  const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/WeddingPhotos${file.name}`;
 
   const reader = new FileReader();
 
-  reader.onload = async function (event) {
-    const photoData = event.target.result.split(",")[1]; // Extract the base64-encoded content
+  reader.onload = function (event) {
+    const photoData = event.target.result.split(',')[1]; // Extract the base64-encoded content
 
     const data = {
-      message: "Upload photo",
+      message: 'Upload photo',
       content: photoData,
     };
 
-    console.log("Hey");
-
-    const response = await fetch(apiUrl, {
+    fetch(apiUrl, {
       method: 'PUT',
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -37,14 +35,18 @@ async function uploadPhoto() {
         Accept: 'application/vnd.github.v3+json',
       },
       body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
-      console.log('Photo uploaded successfully!');
-      window.location.href = 'index.html'; // Redirect to index.html
-    } else {
-      console.error('Error uploading photo:', response.statusText);
-    }
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Photo uploaded successfully!');
+          window.location.href = 'weddingGallery.html'; // Redirect to the success page
+        } else {
+          console.error('Error uploading photo:', response.statusText);
+        }
+      })
+      .catch(error => {
+        console.error('Error uploading photo:', error);
+      });
   };
 
   reader.readAsDataURL(file);
