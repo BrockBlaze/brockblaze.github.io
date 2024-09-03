75cb9c324a895400537f86633dadd8a7961c7752
BrockBlaze <brockblazzard@gmail.com>
2024-09-03 15:47:56 -0600

    a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var d = new Date();
+document.getElementById("year").innerHTML = d.getFullYear();
+
 function changeImg(imgs) {
   var Fimg = document.getElementById("fImg");
   var imgD = document.getElementById("imgDesc");
