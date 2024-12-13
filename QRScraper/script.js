2a5e84aca1c025d4de102256f8ed3df511d09f37
BrockBlaze <brockblazzard@gmail.com>
2024-12-13 12:23:20 -0700

    a

--- QRScraper/script.js
+++ QRScraper/script.js
@@ -8,30 +8,6 @@ const labelName = document.getElementById('labelNameText');
 const labelIP = document.getElementById('labelIPText');
 const labelEnvironment = document.getElementById('labelEnvironmentText');
 
-// Function to scrape metadata using an external API
-async function fetchUsingScraperAPI(url) {
-    const scraperApiUrl = `https://api.scraperapi.com?api_key=5dfaaab34de8447c615346120bf58543&url=${encodeURIComponent(url)}`;
-
-    try {
-        const response = await fetch(scraperApiUrl);
-        const html = await response.text();
-
-        // Parse the HTML and extract the specific attribute value
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, "text/html");
-
-        // Target the input element with the `data-kwcachedvalue` attribute
-        const inputElement = doc.querySelector("input[data-kwcachedvalue]");
-        const waldoText = inputElement
-            ? inputElement.getAttribute("data-kwcachedvalue")
-            : "Text not found";
-
-        return waldoText;
-    } catch (error) {
-        console.error("Error fetching with ScraperAPI:", error);
-        return "Failed to fetch data.";
-    }
-}
 
 // Function to generate a QR code
 function generateQRCode(url) {
@@ -40,7 +16,7 @@ function generateQRCode(url) {
     });
 }
 
-// Form submission handler
+// Form submission handlera
 form.addEventListener('submit', async (event) => {
     fTitle.style.display = 'none';
     form.style.display = 'none';
