0ff098e2889c014df521502ffaa34af3c29a12dd
BrockBlaze <brockblazzard@gmail.com>
2025-06-07 16:29:54 -0600

    a

---
+++ Games/Solace12/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Solace12-0.1.0";
+const contentToCache = [
+    "Build/Solace12.loader.js",
+    "Build/Solace12.framework.js",
+    "Build/Solace12.data",
+    "Build/Solace12.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
